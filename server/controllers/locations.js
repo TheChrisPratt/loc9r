5e5bd6d102c57988f64f2f9c38fcc1e9d57f17bb
Chris Pratt <chris@anodyzed.com>
2014-02-08 02:10:29 -0800

    Added Location & sign-in pages

--- server/controllers/locations.js
+++ server/controllers/locations.js
@@ -1,14 +1,14 @@
 /* GET 'home' page */
 module.exports.homelist = function (req,res) {
-	res.render("index",{ title: "Home" });
+	res.render("locations-list",{ title: "Home" });
 };
 
 /* GET 'Location Info' page */
 module.exports.locationInfo = function (req,res) {
-	res.render("index",{ title: "Location info" });
+	res.render("location-info",{ title: "Location info" });
 };
 
 /* GET 'Add Review' page */
 module.exports.addReview = function (req,res) {
-	res.render("index",{ title: "Add review" });
+	res.render("location-review-form",{ title: "Add review" });
 };
